e1b72e017c0fc4f2ac134e70abf6a39a5571d17c
Lance Munyao <munyaolance1@gmail.com>
2023-05-14 23:35:10 +0300

    Database Connection
    Connecting to the db using code from the official Ms docs

--- index.php
+++ index.php
@@ -1,7 +1,20 @@
 <?php
+$host = 'me-dawa-server.mysql.database.azure.com';
+$username = 'rootGuest';
+$password = 'mysql123lance';
+$db_name = 'laravel';
 
-$con = mysqli_init();
-mysqli_ssl_set($con,NULL,NULL, "{path to CA cert}", NULL, NULL);
-mysqli_real_connect($conn, "me-dawa-server.mysql.database.azure.com", "iafgzihhqu", "mysql123lance", "me-dawa-database", 3306, MYSQLI_CLIENT_SSL);
+//Initializes MySQLi
+$conn = mysqli_init();
 
+mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/DigiCertGlobalRootG2.crt.pem", NULL, NULL);
+
+// Establish the connection
+mysqli_real_connect($conn, 'me-dawa-server.mysql.database.azure.com', 'me-dawa-server', 'mysql123lance', 'quickstartdb', 3306, NULL, MYSQLI_CLIENT_SSL);
+
+//If connection failed, show the error
+if (mysqli_connect_errno())
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
 ?> 

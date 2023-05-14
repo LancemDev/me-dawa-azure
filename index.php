e7bc3163d449dd76ca6fb6b7ebbf104663784e97
Lance Munyao <munyaolance1@gmail.com>
2023-05-14 20:16:00 +0300

    php to mysql
    Testing connection from mysql to php on azure. Both mysql and php servers are hosted on azure

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
-
-echo "Lance, You are not over doing this school thing.";
-
-
+$servername = "me-dawa-server.mysql.database.azure.com";
+$username = "iafgzihhqu";
+$password = "L6702KW7027C6Q8J$";
+$dbname = "me-dawa-database";
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+} else {
+    echo "Connected successfully";
+}
 ?>

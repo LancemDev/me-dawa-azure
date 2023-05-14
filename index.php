83a87916e5feea58f0f986711084a26f9d687250
Lance Munyao <munyaolance1@gmail.com>
2023-05-14 21:30:24 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,12 +3,12 @@ $servername = "me-dawa-server.mysql.database.azure.com";
 $username = "iafgzihhqu";
 $password = "L6702KW7027C6Q8J$";
 $dbname = "me-dawa-database";
-//$conn = mysqli_connect($servername, $username, $password, $dbname);
-//if (!$conn) {
-  //  echo "Not connected";
-//} else {
-  //  echo "Connected successfully";
-//}
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+if (!$conn) {
+    echo "Not connected";
+} else {
+    echo "Connected successfully";
+}
 
 echo $dbname;
 ?>

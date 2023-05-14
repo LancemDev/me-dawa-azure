eadfedde2832cadfeaa91f485db251309e53de13
Lance Munyao <munyaolance1@gmail.com>
2023-05-14 22:32:12 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
-$servername = "me-dawa-server.mysql.database.azure.com";
-$username = "iafgzihhqu";
-$password = "L6702KW7027C6Q8J$";
-$dbname = "me-dawa-database";
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+
+$conn = mysqli_init();
+mysqli_ssl_set($conn,NULL,NULL, "https://1drv.ms/u/s!AtSaJh0u6lj7geNRd0OoESu4kc5fNg?e=5TSv0y", NULL, NULL);
+mysqli_real_connect($conn, "me-dawa-server.mysql.database.azure.com", "rootGuest@me-dawa-server", "mysql123lance", "me-dawa-database", 3306, MYSQLI_CLIENT_SSL, MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT);
 if (!$conn) {
-    echo "Not connected";
-} else {
-    echo "Connected successfully";
+    die("Connection failed: " . mysqli_connect_error());
+} else{
+    echo "Connection to MySQL server " . mysqli_get_host_info($conn) . "<br>";
 }
 
 echo $dbname;

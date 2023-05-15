56203a07949d55e1af8a7ee678aaf356ba7633b2
Lance Munyao <munyaolance1@gmail.com>
2023-05-15 07:08:31 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <?php
 //If connection failed, show the error
-$con = mysqli_init();
-mysqli_ssl_set($con,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-mysqli_real_connect($conn, "me-dawa-server.mysql.database.azure.com", "iafgzihhqu", "mysql123lance", "laravel", 3306, MYSQLI_CLIENT_SSL);
 
-if (mysqli_connect_errno())
-{
-    die('Failed to connect to MySQL: '.mysqli_connect_error());
-}else{
+try{
+    $conn = new PDO("sqlsrv:server = tcp:me-dawa-server.mysql.database.azure.com, 3306; Database = laravel", "iafgzihhqu", "mysql123lance");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "we are getting some progress bruuv";
 }
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
 
 ?> 

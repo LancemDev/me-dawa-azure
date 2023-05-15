267d7170131bb694e86446985dc4a7eb7cb80412
Lance Munyao <74910038+LancemDev@users.noreply.github.com>
2023-05-15 06:58:06 +0000

    Digicert path

--- index.php
+++ index.php
@@ -13,4 +13,9 @@ if (mysqli_connect_errno())
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
+
+$con = mysqli_init();
+mysqli_ssl_set($con,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+mysqli_real_connect($conn, "me-dawa-server.mysql.database.azure.com", "iafgzihhqu", "mysql123lance", "laravel", 3306, MYSQLI_CLIENT_SSL);
+
 ?> 

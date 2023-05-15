12aea9ab07c5f005c05bc8b38d926d5a96dd2077
Lance Munyao <munyaolance1@gmail.com>
2023-05-15 07:04:24 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,14 @@
 <?php
-
-//Initializes MySQLi
-$conn = mysqli_init();
-
-mysqli_ssl_set($conn,NULL,NULL, "", NULL, NULL);
-
-// Establish the connection
-mysqli_real_connect($conn, 'me-dawa-server.mysql.database.azure.com', 'iafgzihhqu@me-dawa-server', 'mysql123lance', 'laravel', 3306, NULL, MYSQLI_CLIENT_SSL);
-
 //If connection failed, show the error
-if (mysqli_connect_errno())
-{
-    die('Failed to connect to MySQL: '.mysqli_connect_error());
-}
-
 $con = mysqli_init();
 mysqli_ssl_set($con,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL);
 mysqli_real_connect($conn, "me-dawa-server.mysql.database.azure.com", "iafgzihhqu", "mysql123lance", "laravel", 3306, MYSQLI_CLIENT_SSL);
+
+if (mysqli_connect_errno())
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}else{
+    echo "we are getting some progress bruuv";
+}
 
 ?> 
